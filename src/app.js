affc7e7e7839b370a1ce7fa5d69cb1864ccd6819
Belzie <dinh.belinda@gmail.com>
2022-09-29 11:52:10 +1000

    Planning Date Element whilst formatting from API data

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+function formatDate(timestamp) {
+  //calculate the data given to give proper date
+  return "Wednesday 29 September 11:45";
+}
+
 function displayTemp(response) {
   console.log(response.data);
   let mainTempElement = document.querySelector(".main-temp");
@@ -6,12 +11,14 @@ function displayTemp(response) {
   let windElement = document.querySelector("#wind");
   let maxTempElement = document.querySelector("#main-overview-max");
   let minTempElement = document.querySelector("#main-overview-min");
+  let dateElement = document.querySelector("#date");
   mainTempElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
   minTempElement.innerHTML = Math.round(response.data.main.temp_min);
+  dateElement.innerHtml = formatDate(response.data.dt * 1000);
 }
 let apiKey = "4b3503b2f08a729413c4d33ef1186004";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Osaka&appid=${apiKey}&units=metric`;
