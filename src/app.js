aa5f708be618e57e90388ec1a1b27955c5c91625
Belzie <dinh.belinda@gmail.com>
2022-09-29 10:34:49 +1000

    Added classes and IDs. Created inner.HTML links to API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 function displayTemp(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
   let mainTempElement = document.querySelector(".main-temp");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let maxTempElement = document.querySelector("#main-overview-max");
+  let minTempElement = document.querySelector("#main-overview-min");
   mainTempElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
 }
 let apiKey = "4b3503b2f08a729413c4d33ef1186004";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Osaka&appid=${apiKey}&units=metric`;
